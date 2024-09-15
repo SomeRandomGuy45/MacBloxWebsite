a1bae75c8cd340505bc7f70e790197494a2a6025
SomeRandomGuy45 <dragonlionmusic@gmail.com>
2024-09-14 20:31:45 -0500

    added more api thats coming soon

--- src/components/DeprecatedBadge.js
+++ src/components/DeprecatedBadge.js
@@ -3,13 +3,40 @@
 import React from 'react';
 import styles from './DeprecatedBadge.module.css'; // Import CSS for styling
 
-const DeprecatedBadge = () => {
-  return (
-    <span className={styles.badge}>
-      <span className={styles.icon}>⚠️</span>
-      <span className={styles.label}>Deprecated</span>
-    </span>
-  );
+function returnSpan(type, customMessage)
+{
+  console.log(customMessage)
+  if (type == "dangerous" != 0 && customMessage.length > 0) 
+  {
+    return (
+      <span className={styles.dangerous_badge}>
+          <span className={styles.icon}>⚠️</span>
+          <span className={styles.label}>{customMessage}</span>
+      </span>
+    );
+  }
+  else if (customMessage.length > 0)
+  {
+    return (
+      <span className={styles.badge}>
+          <span className={styles.icon}>⚠️</span>
+          <span className={styles.label}>customMessage</span>
+      </span>
+    );
+  }
+  else
+  {
+    return (
+      <span className={styles.badge}>
+        <span className={styles.icon}>⚠️</span>
+        <span className={styles.label}>Deprecated</span>
+      </span>
+    );
+  }
+}
+
+const DeprecatedBadge = (type) => {
+  return returnSpan(type.type, type.customMessage);
 };
 
 export default DeprecatedBadge;
